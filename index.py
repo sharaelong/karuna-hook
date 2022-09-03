c6bd916242297d270030f0a021ff8556c959bc4d
Kijun Shin <sharaelong.shin@gmail.com>
2022-09-04 01:02:14 +0900

    Move time interval to env variable

--- index.py
+++ index.py
@@ -12,6 +12,7 @@ notion_version = os.getenv('NOTION_VERSION')
 daily_goal = int(os.getenv('DAILY_GOAL'))
 discord_login_token = os.getenv('DISCORD_LOGIN_TOKEN')
 karuna_id = os.getenv('KARUNA_ID')
+time_interval = int(os.getenv('TIME_INTERVAL'))
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -29,7 +30,7 @@ response = response.json()
 last_edited_time = response['results'][daily_goal-1]['last_edited_time']
 target_date = datetime.fromisoformat(last_edited_time[:-1])
 curr_date = datetime.utcnow()
-if ((curr_date-target_date).seconds >= 86400):
+if ((curr_date-target_date).seconds >= time_interval):
     print('Sometime we just need a break.')
     client.run(discord_login_token)
     print('But the robot did its job.')
