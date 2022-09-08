83d0dde9aee1d2c85e2ac0e17f4fe643b6beec48
Kijun Shin <sharaelong.shin@gmail.com>
2022-09-08 20:48:04 +0900

    Send message as discord embed message

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import datetime
 
 from dotenv import load_dotenv
@@ -14,15 +15,7 @@ discord_login_token = os.getenv('DISCORD_LOGIN_TOKEN')
 karuna_id = os.getenv('KARUNA_ID')
 time_interval = int(os.getenv('TIME_INTERVAL'))
 
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    user = await client.fetch_user(karuna_id)
-    await user.send('Required amount of solutions ain\'t uploaded!')
-    await client.close()
-
+# Check Notion documents
 response = requests.post('https://api.notion.com/v1/databases/' + notion_database_id + '/query',
                          headers={'Authorization': 'Bearer ' + notion_auth_token,
                                   'Notion-Version': notion_version})
@@ -30,11 +23,37 @@ response = response.json()
 last_edited_time = response['results'][daily_goal-1]['last_edited_time']
 target_date = datetime.fromisoformat(last_edited_time[:-1])
 curr_date = datetime.utcnow()
-if ((curr_date-target_date).seconds >= time_interval):
-    print('Sometime we just need a break.')
-    client.run(discord_login_token)
-    print('But the robot did its job.')
-else:
-    print('One more step on the great journey.')
-    
+is_success = (curr_date-target_date).seconds <= time_interval
 
+# Get solved.ac problems
+response = requests.get('https://solved.ac/api/v3/user/problem_stats?handle=jhwest2').json()
+platinum_cnt = 0
+solved_platinum_cnt = 0
+for i in range(16, 21):
+    platinum_cnt += response[i]['total']
+    solved_platinum_cnt += response[i]['solved']
+ratio = 100 * solved_platinum_cnt / platinum_cnt
+
+# Send Message
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    try:
+        user = await client.fetch_user(karuna_id)
+        title = "Today's goal achieved!" if is_success else "Today's goal failed..."
+        description = curr_date.strftime('Date: %Y-%m-%d (%a)')
+        color = 0x4BFD2F if is_success else 0xF81B1B
+        footer = 'One more step on the great journey.' if is_success else 'Sometime we just need a break. But the robot did its job.'
+        embed = discord.Embed(title=title, description=description, color=color)
+        embed.add_field(name="Details", value="{0} / {1} solved. {2:0.2f}% completed.".format(solved_platinum_cnt, platinum_cnt, ratio))
+        embed.set_footer(text=footer)
+    
+        await user.send(embed=embed)
+        await client.close()
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+client.run(discord_login_token)
